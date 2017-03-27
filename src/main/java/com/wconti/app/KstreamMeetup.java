e6a3f4c8dbccbe1a56dad0d0a56f2a6d46cbfe61
wconti <wconti@ippon.fr>
2017-03-27 14:11:54 +0200

    modification du nom du topic

--- src/main/java/com/wconti/app/KstreamMeetup.java
+++ src/main/java/com/wconti/app/KstreamMeetup.java
@@ -26,7 +26,6 @@ public class KstreamMeetup {
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "my-first-streams-application");
         props.put(StreamsConfig.CLIENT_ID_CONFIG, "Example-Processor-Job");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(StreamsConfig.ZOOKEEPER_CONNECT_CONFIG, "localhost:2181");
         props.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         props.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         props.put(StreamsConfig.NUM_STREAM_THREADS_CONFIG, 10);
@@ -39,14 +38,14 @@ public class KstreamMeetup {
         KStreamBuilder builder = new KStreamBuilder();
         JsonParser jsonParser = new JsonParser();
 
-        KStream<String,String> stream = builder.stream("test");
+        KStream<String,String> stream = builder.stream("meetup");
 
            stream.map((key,value) -> {
                 JsonObject meetup = jsonParser.parse(value.toString()).getAsJsonObject();
                 String city = meetup.get("group").getAsJsonObject().get("group_city").getAsString();
                 return KeyValue.pair(city,city);
             }).countByKey("table")
-        .to(stringSerde,longSerde,"testout");
+        .to(stringSerde,longSerde,"meetupout");
 
         KafkaStreams streams = new KafkaStreams(builder, streamingConfig);
         streams.start();
